3370a6e493df88786e6c68c5ad68e7fbd7623e5c
Fabrício Carneiro <fabricioffcr7@gmail.com>
2024-05-31 11:25:35 -0300

    Sistema de Cadastro de Produtos

--- LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -8,6 +8,7 @@
  * @author Adm
  */
 
+import java.sql.SQLException;
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
@@ -23,9 +24,19 @@ public class ProdutosDAO {
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
-        
-        
+        String sql = ("Insert into produtos (nome, valor, status) values (?, ?, ?)");
         conn = new conectaDAO().connectDB();
+        try{
+            prep = conn.prepareStatement(sql);
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.setString(3, produto.getStatus());
+            prep.execute();
+            
+            JOptionPane.showMessageDialog(null, "Produto Adicionado com sucesso!");
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Produto não Adicionado. Tente novamente!");
+        }
         
         
     }
